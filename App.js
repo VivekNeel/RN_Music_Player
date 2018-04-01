015422670ba7b731cebd736068130a6e960830a8
VivekNeel <vivek2neel@gmail.com>
2018-04-01 15:39:13 +0530

    Added tracker capabilities

--- App.js
+++ App.js
@@ -6,12 +6,49 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import TrackPlayer from 'react-native-track-player';
 
 import Pages from './src/index';
+import { AppState } from 'react-native';
 
-export default class App extends Component<Props> {
+import configureStore from './src/redux/configureStore';
+import { updatePlayback } from './src/redux/actions/playerActions';
+
+const store = configureStore();
+
+class App extends Component<Props> {
+  async componentDidMount() {
+    AppState.addEventListener('change', this._handleStateChange);
+
+    await TrackPlayer.setupPlayer({});
+    TrackPlayer.updateOptions({
+      capabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+        TrackPlayer.CAPABILITY_SEEK_TO,
+        TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+        TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      ],
+    });
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleStateChange);
+  }
+
+  _handleStateChange(appState) {
+    if (appState == 'active') {
+      store.dispatch(updatePlayback());
+    }
+  }
+
   render() {
-    return <Pages />;
+    return (
+      <Provider store={store}>
+        <Pages />
+      </Provider>
+    );
   }
 }
 
@@ -33,3 +70,8 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+module.exports = function(store) {
+  App.store = store;
+  return App;
+};
