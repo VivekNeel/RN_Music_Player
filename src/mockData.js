b83ceae99534df302a5a1177b5fd7dbd6308eeba
VivekNeel <vivek2neel@gmail.com>
2018-04-01 08:05:47 +0530

    Added google dummy provider data

---
+++ src/mockData.js
@@ -0,0 +1,211 @@
+const Artists = [
+  {
+    name: 'Album One',
+    background: 'http://storage.googleapis.com/automotive-media/album_art.jpg',
+    songs: [
+      {
+        title: 'Jazz in Paris',
+        album: 'Jazz & Blues',
+        artist: 'Media Right Productions',
+        genre: 'Jazz & Blues',
+        url: 'http://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art.jpg',
+        id: 1,
+        totalTrackCount: 6,
+        duration: 103,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'The Messenger',
+        album: 'Jazz & Blues',
+        artist: 'Silent Partner',
+        genre: 'Jazz & Blues',
+        url: 'http://storage.googleapis.com/automotive-media/The_Messenger.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art.jpg',
+        id: 2,
+        totalTrackCount: 6,
+        duration: 132,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'Talkies',
+        album: 'Jazz & Blues',
+        artist: 'Huma-Huma',
+        genre: 'Jazz & Blues',
+        source: 'http://storage.googleapis.com/automotive-media/Talkies.mp3',
+        albumIimage:
+          'http://storage.googleapis.com/automotive-media/album_art.jpg',
+        id: 3,
+        totalTrackCount: 6,
+        duration: 162,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'On the Bach',
+        album: 'Cinematic',
+        artist: 'Jingle Punks',
+        genre: 'Cinematic',
+        url: 'http://storage.googleapis.com/automotive-media/On_the_Bach.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art.jpg',
+        id: 4,
+        totalTrackCount: 6,
+        duration: 66,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'The Story Unfolds',
+        album: 'Cinematic',
+        artist: 'Jingle Punks',
+        genre: 'Cinematic',
+        url:
+          'http://storage.googleapis.com/automotive-media/The_Story_Unfolds.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art.jpg',
+        id: 5,
+        totalTrackCount: 6,
+        duration: 91,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+    ],
+  },
+  {
+    name: 'Album 2',
+    background:
+      'http://storage.googleapis.com/automotive-media/album_art_2.jpg',
+    songs: [
+      {
+        title: 'Drop and Roll',
+        album: 'Youtube Audio Library Rock',
+        artist: 'Silent Partner',
+        genre: 'Rock',
+        url: 'http://storage.googleapis.com/automotive-media/Drop_and_Roll.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_2.jpg',
+        id: 6,
+        totalTrackCount: 7,
+        duration: 121,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'Motocross',
+        album: 'Youtube Audio Library Rock',
+        artist: 'Topher Mohr and Alex Elena',
+        genre: 'Rock',
+        url: 'http://storage.googleapis.com/automotive-media/Motocross.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_2.jpg',
+        id: 8,
+        totalTrackCount: 7,
+        duration: 182,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: "Wish You'd Come True",
+        album: 'Youtube Audio Library Rock',
+        artist: 'The 126ers',
+        genre: 'Rock',
+        url:
+          'http://storage.googleapis.com/automotive-media/Wish_You_d_Come_True.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_2.jpg',
+        id: 8,
+        totalTrackCount: 7,
+        duration: 169,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'Awakening',
+        album: 'Youtube Audio Library Rock',
+        artist: 'Silent Partner',
+        genre: 'Rock',
+        url: 'http://storage.googleapis.com/automotive-media/Awakening.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_2.jpg',
+        id: 9,
+        totalTrackCount: 7,
+        duration: 220,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'Home',
+        album: 'Youtube Audio Library Rock',
+        artist: 'Letter Box',
+        genre: 'Rock',
+        url: 'http://storage.googleapis.com/automotive-media/Home.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_2.jpg',
+        id: 10,
+        totalTrackCount: 7,
+        duration: 213,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'Tell The Angels',
+        album: 'Youtube Audio Library Rock',
+        artist: 'Letter Box',
+        genre: 'Rock',
+        url:
+          'http://storage.googleapis.com/automotive-media/Tell_The_Angels.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_2.jpg',
+        trackNumber: 6,
+        totalTrackCount: 7,
+        duration: 208,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'Hey Sailor',
+        album: 'Youtube Audio Library Rock',
+        artist: 'Letter Box',
+        genre: 'Rock',
+        url: 'http://storage.googleapis.com/automotive-media/Hey_Sailor.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_2.jpg',
+        id: 11,
+        totalTrackCount: 7,
+        duration: 193,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+    ],
+  },
+  {
+    name: 'Album 3',
+    background:
+      'http://storage.googleapis.com/automotive-media/album_art_3.jpg',
+    songs: [
+      {
+        title: 'Keys To The Kingdom',
+        album: 'Youtube Audio Library Rock 2',
+        artist: 'The 126ers',
+        genre: 'Rock',
+        url:
+          'http://storage.googleapis.com/automotive-media/Keys_To_The_Kingdom.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_3.jpg',
+        id: 12,
+        totalTrackCount: 2,
+        duration: 221,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+      {
+        title: 'The Coldest Shoulder',
+        album: 'Youtube Audio Library Rock 2',
+        artist: 'The 126ers',
+        genre: 'Rock',
+        url:
+          'http://storage.googleapis.com/automotive-media/The_Coldest_Shoulder.mp3',
+        albumImage:
+          'http://storage.googleapis.com/automotive-media/album_art_3.jpg',
+        id: 13,
+        totalTrackCount: 2,
+        duration: 160,
+        site: 'https://www.youtube.com/audiolibrary/music',
+      },
+    ],
+  },
+];
+
+module.exports = { Artists: Artists };
