98ce877ec168333a2174a0b12d24a9ffd92d9970
VivekNeel <vivek2neel@gmail.com>
2018-04-02 02:19:48 +0530

    Generating apk.

--- android/app/src/main/java/com/rnmusic/MainApplication.java
+++ android/app/src/main/java/com/rnmusic/MainApplication.java
@@ -3,7 +3,6 @@ package com.rnmusic;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.brentvatne.react.ReactVideoPackage;
 import guichaguri.trackplayer.TrackPlayer;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactVideoPackage(),
             new TrackPlayer(),
             new VectorIconsPackage()
       );
