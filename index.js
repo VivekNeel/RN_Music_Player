eba9ec859dc0f92b7f4e9a6e5dffcfa73c6a8675
VivekNeel <vivek2neel@gmail.com>
2018-04-01 07:19:16 +0530

    Registering player using event handler

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import createApp from './App';
 import AlbumList from './src/components/AlbumList';
 import createEventHandler from './src/player/eventhandler';
 import TrackPlayer from 'react-native-track-player';
 
-AppRegistry.registerComponent('rnmusic', () => App);
-TrackPlayer.registerEventHandler(createEventHandler());
+import configureStore from './src/redux/configureStore';
+
+const store = configureStore();
+
+AppRegistry.registerComponent('rnmusic', () => createApp(store));
+
+TrackPlayer.registerEventHandler(createEventHandler(store));
